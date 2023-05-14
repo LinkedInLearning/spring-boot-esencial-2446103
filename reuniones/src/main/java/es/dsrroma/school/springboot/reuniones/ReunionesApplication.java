0dd130b34feda4e122b6f2244470eb4cddc8709b
Mariona Nadal <70766382+DSRschool@users.noreply.github.com>
2023-05-14 18:13:20 +0200

    01_03 Inicialización de proyectos Spring Boot

---
+++ reuniones/src/main/java/es/dsrroma/school/springboot/reuniones/ReunionesApplication.java
@@ -0,0 +1,13 @@
+package es.dsrroma.school.springboot.reuniones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReunionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReunionesApplication.class, args);
+	}
+
+}
---
+++ reuniones/src/test/java/es/dsrroma/school/springboot/reuniones/ReunionesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.dsrroma.school.springboot.reuniones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReunionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

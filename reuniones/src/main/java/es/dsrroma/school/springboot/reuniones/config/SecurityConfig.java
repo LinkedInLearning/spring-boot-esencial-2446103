25af75937b3965acd16e11c6acfc22cff5b71e34
Mariona Nadal <70766382+DSRschool@users.noreply.github.com>
2023-05-15 19:57:34 +0200

    05_02 Configuración de Spring Security

---
+++ reuniones/src/main/java/es/dsrroma/school/springboot/reuniones/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package es.dsrroma.school.springboot.reuniones.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests().requestMatchers("/api/*").hasRole("API_USER") //
+				.and().authorizeHttpRequests().requestMatchers("/*").authenticated() //
+				.and().formLogin().loginPage("/login").permitAll() //
+				.and().logout().permitAll()
+				;
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails usuario1 = User.builder().username("granJefe")
+				.password(passwordEncoder().encode("ssshhh"))
+				.roles("USER", "API_USER").build();
+		UserDetails usuario2 = User.builder().username("currito")
+				.password(passwordEncoder().encode("pasapasa"))
+				.roles("USER").build();
+		return new InMemoryUserDetailsManager(usuario1, usuario2);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
